62d58eaac06bad4adbceee8919fdef74296ad004
TheZD <sana@TheZDui-MacBookPro.local>
2024-01-02 10:19:44 +0900

    error fixed

--- kkang-kkang/src/App.tsx
+++ kkang-kkang/src/App.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Promotion from "./pages/Promotion";
 import GlobalStyle from "./styles/global";
 import KeyPair from "./pages/key-pair";
-import Test from "./pages/Test";
 import { easterEgg } from "./atoms/state";
 import PromotionPage from "./pages/Promotion/Promotion";
 import NewTx from "./pages/New-tx";
@@ -23,7 +22,6 @@ function App() {
           <Route path="/" element={ea ? <Promotion /> : <PromotionPage />} />
           <Route path="/new-tx" element={<NewTx />} />
           <Route path="/key-pair" element={<KeyPair />} />
-          <Route path="/test" element={<Test />} />
           <Route path="/find-tx" element={<FindTx />} />
           <Route path="/find-block" element={<FindBlock />} />
           <Route path="/list-block" element={<ListBlock />} />
